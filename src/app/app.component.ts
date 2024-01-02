362e90ba01013f0e8751b61d76a66f9b3f41ba1e
Jim Mortenson <nosnetrom@gmail.com>
2024-01-01 20:05:04 -0600

    Part 1 of tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,33 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-todo';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'laugh', done: false },
+  ];
+
+  get items() {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+      done: false,
+    });
+  }
 }
